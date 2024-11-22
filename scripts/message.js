313e2b17a1cc2e9e4862e32e559504bb0ecd47cf
vjamespd <dvinardjames@gmail.com>
2024-11-23 04:27:51 +0800

    Designed Message page

--- scripts/message.js
+++ scripts/message.js
@@ -1,3 +1,10 @@
-const greetings = "Hello Javascript";
-
-document.getElementById("demo").innerHTML = greetings;
+// script.js
+// when input is clicked, placeholder will removed
+document.getElementById('message').addEventListener('focus', function() {
+    this.setAttribute('placeholder', '');
+  });
+  
+  document.getElementById('message').addEventListener('blur', function() {
+    this.setAttribute('placeholder', 'Message');
+  });
+  
